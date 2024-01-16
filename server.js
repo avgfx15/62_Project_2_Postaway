e3ae587eb4a75a5f4e44d2f6e34b23ba3d966548
Ajay Gandhi <avgfx15@gmail.com>
2024-01-16 19:02:36 +0530

    Create class, and function for customErrorHandler, Logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import swagger from 'swagger-ui-express';
 import swaggerjson from './swagger.json' assert {type: 'json'};
+import { customErrorHandler, errorHandlerMiddleware } from './src/errorHandler/errorHandler.js';
 
 const server = express();
 
@@ -14,11 +15,22 @@ server.use('/apidoc', swagger.serve, swagger.setup(swaggerjson))
 
 
 
+server.get("/test-custom-error", (req, res) => {
+    throw new customErrorHandler(
+        505,
+        "testing app level custom error handling middleware"
+    );
+});
 
 //? Defaule Route Definition
 server.get('/', (req, res) => {
-    res.send('Postaway Application Home Page')
+    throw new customErrorHandler(200, 'Postaway Application Home Page')
+    // res.send('Postaway Application Home Page')
 });
+
+//? Error Handler and Logger Middleware setup
+
+server.use(errorHandlerMiddleware);
 
 // ? Listern Route Definition
 
--- src/errorHandler/errorHandler.js
+++ src/errorHandler/errorHandler.js
@@ -1,8 +1,26 @@
+import { logger } from "../middlewares/loggerMiddleware.js";
+
 // $ Custom Error Handler Class
 export class customErrorHandler extends Error {
-    constructor(message, statusCode) {
+    constructor(statusCode, message) {
         super(message);
         this.statusCode = statusCode;
     }
 }
 
+// $ Create ErrorMiddleware Function
+
+export const errorHandlerMiddleware = (err, req, res, next) => {
+    console.log('Logger Function call');
+
+    // ? Logger setup to print logs in log file
+
+    const loggerData = `timestemp : ${new Date().toString()}, requestUrl : ${req.url}, statusCode : ${err.statusCode}, message : ${err.message}`;
+    logger.info(loggerData);
+    logger.error(loggerData)
+
+    if (err instanceof customErrorHandler) {
+        return res.status(err.statusCode).send(err.message);
+    }
+    return res.status(500).send('Server Error - Something went wrong');
+}
